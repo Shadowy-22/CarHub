d8c0f8a22c7e93f36ae86169e1753b6314bb8824
Juan Bautista Aguilar <juanbautistaaguilar21@gmail.com>
2024-01-25 16:49:10 -0300

    Initial Configuration

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">Carhub</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
